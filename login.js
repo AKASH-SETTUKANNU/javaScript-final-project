8f8baa28f07ff796264cb484ad49fc4d879768c6
Akash Settukannu <akash.settukannu@claysys.com>
2024-09-01 13:03:34 +0530

    add code

--- login.js
+++ login.js
@@ -1,1 +1,63 @@
-let 
+let email = document.getElementById("user-mail");
+let password = document.getElementById("user-password");
+let mailError = document.getElementById("email-error");
+let passwordError = document.getElementById("password-error");
+
+//handel email error here
+function emailErrorHandel() {
+   if (email.value.trim() === '') {
+      mailError.textContent = "Please enter your Email...";
+      return false;
+   } else if (!emailExistornot()) { 
+      mailError.textContent = "User not exist, please signup...";
+      return false;
+   } else {
+      mailError.textContent = "";
+      return true;
+   }
+}
+
+// Function to check if the email exists
+function emailExistornot() {
+   let users = JSON.parse(localStorage.getItem("users") || "[]");
+   return users.some((user) => user.userEmail === email.value); 
+}
+
+//check pass password in local storage match or not
+function passWordMatch() {
+   let users = JSON.parse(localStorage.getItem("users") || "[]");
+   let user = users.find((user) => user.userEmail === email.value); 
+   if (user) {
+      return user.userPassword === password.value; 
+   }
+   return false;
+}
+
+//password error handel
+function passwordErrorHandel() {
+   if (password.value.trim() === '') {
+      passwordError.textContent = "Please enter your password...";
+      return false;
+   } else if (!passWordMatch()) { 
+      passwordError.textContent = "Password does not match...";
+      return false;
+   } else {
+      passwordError.textContent = "";
+      return true;
+   }
+}
+
+//login user if login button tiger
+function loginUser(event) {
+   event.preventDefault(); 
+
+   if (emailErrorHandel() && passwordErrorHandel()) { 
+      let users = JSON.parse(localStorage.getItem("users") || "[]");
+      let user = users.find((user) => user.userEmail === email.value); 
+      if (user) {
+         let name = user.userName;
+         window.location.href = "index.html";
+         alert(`Welcome, ${name}!`);
+      }
+   }
+}
